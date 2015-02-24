500129b361fd4e7dce0412cd2c7551d455b603a3
Alex Tsankov <antsankov@gmail.com>
2015-02-24 00:14:31 +0000

    code structure

--- brofiler.py
+++ brofiler.py
@@ -32,6 +32,20 @@ class system_config(object):
                 bro_script)
             local_bro_file.write(local_string)
 
+def broctl_install():
+    subprocess.check_output(
+        'sudo /usr/local/bro/bin/broctl install',
+        shell=True)
+
+def broctl_restart():
+    subprocess.check_output(
+        'sudo /usr/local/bro/bin/broctl restart',
+        shell=True)
+
+def broctl_refresh():
+    broctl_install()
+    broctl_restart()
+
 class bro_device(object):
 
     """ Used to define the type of bro device we are going to be spinning up. We can create either mangers, proxies, or workers. Only workers require interface information
@@ -110,80 +124,6 @@ class top(object):
     def csv_format(self):
         return "{},{},{},{},{},{},{},{},{}\n".format(self.time,self.name,self.role,self.host,self.pid,self.proc,self.vsize,self.rss,self.cpu)
 
-class netstat(object):
-
-    """Output of each device reutrned by 'broctl netstats':
-
-    bro: 1423861198.685558 recvd=25118568 dropped=69563523 link=94682096
-
-    Contains information about the following-
-
-        device : what is the name of the device, defined in node.cfg
-        time: the time that the measurment was taken, in Unix epoc time
-        recvd: this is the number of packets that have been successfully analyzed
-        dropped: these are packets that are dropped due to lack of resources
-        link: these are the total number of packets that have been recieved, regardless of whether they have been analyzed
-    """
-
-    def __init__(self, device, time, recvd, dropped, link):
-        self.device = device
-        self.time = time
-        self.recvd = float(recvd)
-        self.dropped = float(dropped)
-        self.link = float(link)
-        self.success_rate = self.recvd / self.link
-        self.loss_rate = self.dropped / self.link
-
-    def confirm(self):
-        """ Sanity check if the number of dropped and recvd matches number of packets on the link """
-        if (self.recvd + self.dropped != self.link):
-            return False
-        else:
-            return True
-
-    def print_all(self):
-        """ Prints all of the useful information conatined in this object, useful for debugging purposes"""
-        print('NETSTAT')
-        print("Device: {}".format(self.device))
-        print("Time: {}".format(self.time))
-        print("Recvd: {}".format(self.recvd))
-        print("Dropped: {}".format(self.dropped))
-        print("Link: {}".format(self.link))
-        print('======')
-    
-    def csv_format(self):
-        return "{},{},{},{},{},{}\n".format(self.time,self.device,self.recvd,self.dropped,self.link,self.success_rate)
-
-class capstat(object):
-
-    """Output of link information returned by 'broctl snapshots', based on a 10s average:
-
-    localhost/eth2        36.3       25.6
-
-    Contains information about the following-
-
-        host/interface : the interface of the device being meauserd (TODO - possibly split these into two other object variables)
-        kpps : 10s avg of number of kpps on the link
-        mbps : 10s avg of speed rate on the link
-
-    """
-
-    def __init__(self, interface, kpps, mbps):
-        self.interface = interface
-        self.kpps = kpps
-        self.mbps = mbps 
-
-    def print_all(self): 
-        print('CAPSTAT')
-        print("Interface: {}".format(self.interface))
-        print("kpps: {}".format(self.kpps))
-        print("mbps: {}".format(self.mbps))
-        print('------')
-
-    def csv_format(self):
-        return "{},{},{}\n".format(self.interface, self.kpps, self.mbps)
-
-
 def collect_top():
     
     """ This function reutrns an array of top objects for each bro device returned by 'broctl top'.
@@ -223,8 +163,8 @@ def collect_top():
                     top_split[2],
                     top_split[3],
                     top_split[4],
-                    top_split[5],
-                    top_split[6],
+                    convert_size(top_split[5]),
+                    convert_size(top_split[6]),
                     top_split[7].replace('%','')))
 
         with open("top.csv","a") as f: 
@@ -235,6 +175,66 @@ def collect_top():
 
     except subprocess.CalledProcessError:
         print('FAILED top')
+
+
+def convert_size(size):
+    if "M" in size:
+        return int(size.replace('M','')) * 1000000
+
+    if "K" in size:
+        return int(size.replace('K','')) * 1000
+
+    if "G" in size:
+        return int(size.replace('G','')) * 1000000
+
+
+class netstat(object):
+
+    """Output of each device reutrned by 'broctl netstats':
+
+    bro: 1423861198.685558 recvd=25118568 dropped=69563523 link=94682096
+
+    Contains information about the following-
+
+        device : what is the name of the device, defined in node.cfg
+        time: the time that the measurment was taken, in Unix epoc time
+        recvd: this is the number of packets that have been successfully analyzed
+        dropped: these are packets that are dropped due to lack of resources
+        link: these are the total number of packets that have been recieved, regardless of whether they have been analyzed
+    """
+
+    def __init__(self, device, time, recvd, dropped, link):
+        self.device = device
+        self.time = time
+        self.recvd = float(recvd)
+        self.dropped = float(dropped)
+        self.link = float(link)
+        self.success = self.success_rate() 
+        self.loss = 1 - self.success_rate()
+
+    def success_rate(self):
+        if self.link != 0:
+            return self.recvd / self.link 
+        else:
+            return 1 
+    def confirm(self):
+        """ Sanity check if the number of dropped and recvd matches number of packets on the link """
+        if (self.recvd + self.dropped != self.link):
+            return False
+        else:
+            return True
+
+    def print_all(self): 
+        print('NETSTAT')
+        print("Device: {}".format(self.device))
+        print("Time: {}".format(self.time))
+        print("Recvd: {}".format(self.recvd))
+        print("Dropped: {}".format(self.dropped))
+        print("Link: {}".format(self.link))
+        print('======')
+    
+    def csv_format(self):
+        return "{},{},{},{},{},{}\n".format(self.time,self.device,self.recvd,self.dropped,self.link,self.success)
 
 def collect_netstats():
     
@@ -284,6 +284,35 @@ def collect_netstats():
     except subprocess.CalledProcessError:
         print('FAILED netstats')
 
+class capstat(object):
+
+    """Output of link information returned by 'broctl snapshots', based on a 10s average:
+
+    localhost/eth2        36.3       25.6
+
+    Contains information about the following-
+
+        host/interface : the interface of the device being meauserd (TODO - possibly split these into two other object variables)
+        kpps : 10s avg of number of kpps on the link
+        mbps : 10s avg of speed rate on the link
+
+    """
+
+    def __init__(self,time, interface, kpps, mbps):
+        self.time = time
+        self.interface = interface
+        self.kpps = kpps
+        self.mbps = mbps 
+
+    def print_all(self): 
+        print('CAPSTAT')
+        print("Interface: {}".format(self.interface))
+        print("kpps: {}".format(self.kpps))
+        print("mbps: {}".format(self.mbps))
+        print('------')
+
+    def csv_format(self):
+        return "{},{},{},{}\n".format(self.time,self.interface, self.kpps, self.mbps)
 
 def collect_capstats():
     """This function returns an array of capstat object for each interface returned by 'broctl capstats'
@@ -300,63 +329,48 @@ def collect_capstats():
     capstats_snapshot = []
     # NOTE: capstats seems to output on the stderror FD NOT stdout
    
-    try:
-            capstats_string = subprocess.check_output(
-                'sudo /usr/local/bro/bin/broctl capstats',
-                stderr=subprocess.STDOUT,
-                shell=True)
-            # split capstats into multiple lines
-            capstats_split_line = capstats_string.splitlines()
+    
+    capstats_string = subprocess.check_output(
+        'sudo /usr/local/bro/bin/broctl capstats',
+        stderr=subprocess.STDOUT,
+        shell=True)
+    # split capstats into multiple lines
+    capstats_split_line = capstats_string.splitlines()
 
-            # go through the lines for the different interfaces, starting on the third 
-            for i in range(3, len(capstats_split_line)):
-                # split on the words
-                capstats_split_word = capstats_split_line[i].split()
+    # go through the lines for the different interfaces, starting on the third 
+    for i in range(3, len(capstats_split_line)):
+        # split on the words
+        capstats_split_word = capstats_split_line[i].split()
 
-                if (len(capstats_split_word) is 0):
-                    return
+        if (len(capstats_split_word) is 0):
+            return
 
-                # instatiate a capstats_snapshot for each of the interfaces and add it
-                # to our capstats_snapshot list
-                capstats_snapshot.append(
-                    capstat(
-                        capstats_split_word[0],
-                        capstats_split_word[1],
-                        capstats_split_word[2]))
+        # instatiate a capstats_snapshot for each of the interfaces and add it
+        # to our capstats_snapshot list
+        capstats_snapshot.append(
+            capstat(
+                time.time(),
+                capstats_split_word[0],
+                capstats_split_word[1],
+                capstats_split_word[2]))
 
-            with open("capstats.csv","a") as f: 
-                for device_cap in capstats_snapshot:
-                    f.write(device_cap.csv_format())
-            
-            return capstats_snapshot
-            
-    except: 
-        print("Capstats fail")
+    with open("capstats.csv","a") as f: 
+        for device_cap in capstats_snapshot:
+            f.write(device_cap.csv_format())
+    
+    return capstats_snapshot
+    
 
 def file_init():
 
     with open('netstats.csv', "wb") as netcsv:
-         netcsv.write("Device,Time,Recieved,Dropped,Total,Success\n")
+         netcsv.write("Time,Device,Recieved,Dropped,Total,Success\n")
 
     with open('capstats.csv', "wb") as capcsv: 
-        capcsv.write("Interface/Device,kpps,mbps\n")
+        capcsv.write("Time,Interface/Device,kpps,mbps\n")
 
     with open('top.csv', "wb") as topcsv: 
         topcsv.write("Time,Device,Role,Host,Pid,Proc,VSize,Rss,Cpu\n")
-
-def broctl_install():
-    subprocess.check_output(
-        'sudo /usr/local/bro/bin/broctl install',
-        shell=True)
-
-def broctl_restart():
-    subprocess.check_output(
-        'sudo /usr/local/bro/bin/broctl restart',
-        shell=True)
-
-def broctl_refresh():
-    broctl_install()
-    broctl_restart()
 
 def add_to_db(snapshot,collection):    
     for entry in snapshot:
@@ -377,8 +391,8 @@ def main():
     capstat_c.remove()
 
     starttime = time.time()
-    # broctl_refresh()
-    # file_init()
+    #broctl_refresh()
+    file_init()
 
     test_config = system_config(
         '/usr/local/bro/etc/node.cfg',
