b5e85633b172bc93c4893ebe075bdda9e3a376a0
Alex Tsankov <antsankov@gmail.com>
2015-03-11 17:05:36 +0000

    Properly sized graphs

--- server/static/js/clean.js
+++ server/static/js/clean.js
@@ -1,23 +1,24 @@
-
-//this function takes in an object, converts its data attribute to an array of strings and returns it. 
+//this function takes in an object, converts its data attribute to an array of ints and returns it.
 function clean(object){
-    var ints = []
+    var ints = [];
     object.data.forEach(function(measure){
         ints.push(parseInt(measure))
     })
- 
     object.data = ints;
-    console.log(object.data)
-    return object
+    return object;
 }
 
-//this takes an array of objects, cleans each of their data attributes and returns them as an array. 
+//this takes an array of objects, cleans each of their data attributes and returns them as an array.
 function cleaner(objects){
-    var cleaned_objects = []
+    var cleaned_objects = [];
     objects.forEach(function(object){
-        clean_object = clean(object) 
-        cleaned_objects.push(clean_object)
+        //create a new attribute and delete id. This is so naming works with highcharts
+        object.name = object._id;
+        delete object._id;
+     
+        clean_object = clean(object);
+        cleaned_objects.push(clean_object);
     })
-   return cleaned_objects 
-}
 
+    return cleaned_objects;
+}
