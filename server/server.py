acbec393b822a615ba3523a8eee9a81404e93050
Alex Tsankov <antsankov@gmail.com>
2015-03-11 18:26:42 +0000

    Timestamp query

--- server/server.py
+++ server/server.py
@@ -18,11 +18,12 @@ capstat_c = db.capstat
 def base_page(name=None): 
  
     #this is where we list our timestamp
+    start = arrow.get('2014-05-11T21:23:58.970460+00:00')
     end  = arrow.get('2015-05-11T21:23:58.970460+00:00')
-    test = top_graph(0,end,12)
+    test = top_graph(start,end,12)
     
     #find anything in the top collection before the time stamp
-    return render_template('base.html',test= test,name=name)
+    return render_template('base.html',test= test,name= name)
 
 
 
@@ -31,27 +32,31 @@ class top_graph(object):
     def __init__(self, start_time, end_time, limit):
         
         self.start_time = start_time
-        self.end_time = end_time
+        self.end_time = end_time 
         
-        
-        minimum = 2161728000  
-        maximum = 0
+        self.time_stamps = [] 
 
-        results = []
+        self.minimum = 2161728000  
+        self.maximum = 0
 
-        #for measure in top_c.find({'time' : {'$lte' : end_time.timestamp} }, { '_id': 0,'pid': 1,'cpu': 1,'time':1}).limit(limit):
-        query = top_c.aggregate([{ "$match" : {'time' : {'$lte' : end_time.timestamp}}},{"$group":{"_id": "$identifier" ,'data': { "$push": "$cpu"}}}])
-        
-        for cpu_reading in query['result']: 
-            results.append(cpu_reading)
-
-        print results 
+        for time_stamp in top_c.find({'time' : {"$lte" : end_time.timestamp,"$gte" : start_time.timestamp}}, {'time' : 1, '_id' : 0}):
+            #just gets the the actual time int
+            time = time_stamp['time'] 
             
-        self.results = results
-        self.minimum = minimum 
-        self.maximum = maximum 
+            if time < self.minimum:
+                self.minimum = time
+            
+            if time > self.maximum: 
+                self.maximum = time
 
-
+            self.time_stamps.append(time)
+        
+ 
+        query = top_c.aggregate([{ "$match" : {'time' : {'$lte' : end_time.timestamp,"$gte" : start_time.timestamp}}},{"$group":{"_id": "$identifier" ,'data': { "$push": "$cpu"}}}])
+        
+        self.results = []          
+        for cpu_reading in query['result']: 
+            self.results.append(cpu_reading) 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=80,debug=True)
