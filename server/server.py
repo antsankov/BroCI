03c4c8365a627751281c230f468d531a86c42145
Alex Tsankov <antsankov@gmail.com>
2015-03-20 21:32:29 +0000

    Added in pull capability

--- server/server.py
+++ server/server.py
@@ -19,25 +19,35 @@ capstat_c = db.capstat
 
 # See http://www.masnun.com/2012/01/22/fetching-remote-git-repo-with-python-in-a-few-lines-of-codes.html
 def setup_repo(DIR_NAME,REMOTE_URL):
-    print(os.getcwd())
-    if os.path.isdir(DIR_NAME):
-        shutil.rmtree(DIR_NAME)
 
-    os.mkdir(DIR_NAME)
-
-    repo = git.Repo.init(DIR_NAME)
-    origin = repo.create_remote('origin',REMOTE_URL)
-    origin.fetch()
-    origin.pull(origin.refs[0].remote_head)
+    #this is for a pull
+    if os.path.isdir(DIR_NAME): 
+        repo = git.Repo(DIR_NAME)
+        origin = repo.remotes.origin
+        origin.pull(origin.refs[0].remote_head)
     
+    #this is if we need to remake the directory
+    else:
+        os.mkdir(DIR_NAME)
+        repo = git.Repo.init(DIR_NAME)
+        origin = repo.create_remote('origin',REMOTE_URL)
+        origin.fetch()
+        origin.pull(origin.refs[0].remote_head)
+        
     local = DIR_NAME + '/local.bro'
-    scripts = DIR_NAME + '/scripts/ '
+    repo_scripts = DIR_NAME + '/scripts/ '
 
     #copy the local file from the pulled repo to the local on the system
     shutil.copy(local,'/usr/local/bro/share/bro/site')
     #move the scripts to the proper direcotry as well
     #use instead of shutil, because copytree sucks.  
-    os.system("cp -rf "+ scripts + '/usr/local/bro/share/bro/policy/brofiler') 
+    os.system("cp -rf "+ repo_scripts + '/usr/local/bro/share/bro/policy/scripts') 
+
+def update_repo(REMOTE_URL):
+    repo = git.Repo.init(REMOTE_URL)
+    o = repo.remotes.origin
+    o.pull()    
+    
 
 class capstat_graph(object):
 
