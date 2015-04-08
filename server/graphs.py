d578e239eeb1fd494411f1da91ff0501bed217e1
Alex Tsankov <antsankov@gmail.com>
2015-04-08 19:07:45 +0000

    cleanup done

--- server/graphs.py
+++ server/graphs.py
@@ -69,3 +69,48 @@ class netstat_graph(object):
         for success_rate in success_rate_query['result']:
             self.success_results.append(success_rate)
 
+
+class top_graph(object):
+
+    def __init__(self, start_time, end_time, top_c):
+
+        self.start_time = start_time
+        self.end_time = end_time
+
+        self.time_stamps = Set([])
+
+        self.time_minimum = 2161728000
+        self.time_maximum = 0
+
+        for time_stamp in top_c.find({'time' : {"$lte" : end_time.timestamp,"$gte" : start_time.timestamp}}, {'time' : 1, '_id' : 0}):
+            #just gets the the actual time int
+            time = int(time_stamp['time'])
+
+            if time < self.time_minimum:
+                self.time_minimum = time
+
+            if time > self.time_maximum:
+                self.time_maximum = time
+
+            self.time_stamps.add(time)
+
+        #convert it to a list so it is json serializable
+        self.time_stamps = list(self.time_stamps)
+
+        cpu_query = top_c.aggregate([{ "$match" : {'time' : {'$lte' : end_time.timestamp,"$gte" : start_time.timestamp}}},{"$group":{"_id": "$identifier" ,'data': { "$push": "$cpu"}}}])
+
+        self.cpu_results = []
+        for cpu_reading in cpu_query['result']:
+            self.cpu_results.append(cpu_reading)
+
+        #this gets a little bigger than the largest memory use, so we can add a y axis on our graph
+        self.ram_maximum = 1.2 * top_c.find_one(sort=[("rss",-1)])['rss']
+
+        ram_query = top_c.aggregate([{ "$match" : {'time' : {'$lte' : end_time.timestamp,"$gte" : start_time.timestamp}}},{"$group":{"_id": "$identifier" ,'data': { "$push": "$rss"}}}])
+
+        self.ram_results = []
+        for ram_reading in ram_query['result']:
+            self.ram_results.append(ram_reading)
+
+
+
--- server/server.py
+++ server/server.py
@@ -4,14 +4,11 @@ import arrow
 from flask import *
 from pymongo import *
 from json import dumps
-from sets import Set
 
-
-
-
+#profiler modules
 from profiler_vc import setup_repo, update_repo
 from unit_test import parse_unit, unit_test 
-from graphs import capstat_graph, netstat_graph
+from graphs import capstat_graph, netstat_graph, top_graph
 
 #Initalize Flask
 app = Flask(__name__)
@@ -22,124 +19,15 @@ db = client.brofiler
 top_c = db.top
 netstat_c = db.netstat
 capstat_c = db.capstat
-'''
-class capstat_graph(object):
 
-    def __init__(self, start_time, end_time):
-
-        self.start_time = start_time
-        self.end_time = end_time 
-
-        self.time_stamps = Set([]) 
-
-        self.time_minimum = 2161728000  
-        self.time_maximum = 0 
-
-        for time_stamp in capstat_c.find({'time' : {"$lte" : end_time.timestamp,"$gte" : start_time.timestamp}}, {'time' : 1, '_id' : 0}):
-            #just gets the the actual time int
-            time = int(time_stamp['time']) 
-            
-            if time < self.time_minimum:
-                self.time_minimum = time
-            
-            if time > self.time_maximum: 
-                self.time_maximum = time
-
-            self.time_stamps.add(time)
-
-        #convert it to a list so it is json serializable
-        self.time_stamps = list(self.time_stamps) 
-
-        speed_query = capstat_c.aggregate([{ "$match" : {'time' : {'$lte' : end_time.timestamp,"$gte" : start_time.timestamp}}},{"$group":{"_id": "$identifier" ,'data': { "$push": "$mbps"}}}])
-        
-        self.speed_results = []          
-        for speed in speed_query['result']: 
-            self.speed_results.append(speed) 
-
-        self.speed_maximum = 1.2 * float(capstat_c.find_one(sort=[("mbps",-1)])['mbps'])
-
-class netstat_graph(object):
-
-    def __init__(self, start_time, end_time):
-        
-        self.start_time = start_time
-        self.end_time = end_time 
-        
-        self.time_stamps = Set([]) 
-
-        self.time_minimum = 2161728000  
-        self.time_maximum = 0 
-
-        for time_stamp in netstat_c.find({'time' : {"$lte" : end_time.timestamp,"$gte" : start_time.timestamp}}, {'time' : 1, '_id' : 0}):
-            #just gets the the actual time int
-            time = int(time_stamp['time']) 
-            
-            if time < self.time_minimum:
-                self.time_minimum = time
-            
-            if time > self.time_maximum: 
-                self.time_maximum = time
-
-            self.time_stamps.add(time)
-
-        #convert it to a list so it is json serializable
-        self.time_stamps = list(self.time_stamps) 
-
-        success_rate_query = netstat_c.aggregate([{ "$match" : {'time' : {'$lte' : end_time.timestamp,"$gte" : start_time.timestamp}}},{"$group":{"_id": "$identifier" ,'data': { "$push": "$success"}}}])
-        
-        self.success_results = []          
-        for success_rate in success_rate_query['result']: 
-            self.success_results.append(success_rate)
-'''
-class top_graph(object):
-
-    def __init__(self, start_time, end_time):
-        
-        self.start_time = start_time
-        self.end_time = end_time 
-        
-        self.time_stamps = Set([]) 
-
-        self.time_minimum = 2161728000  
-        self.time_maximum = 0
-
-        for time_stamp in top_c.find({'time' : {"$lte" : end_time.timestamp,"$gte" : start_time.timestamp}}, {'time' : 1, '_id' : 0}):
-            #just gets the the actual time int
-            time = int(time_stamp['time']) 
-            
-            if time < self.time_minimum:
-                self.time_minimum = time
-            
-            if time > self.time_maximum: 
-                self.time_maximum = time
-
-            self.time_stamps.add(time)
-
-        #convert it to a list so it is json serializable
-        self.time_stamps = list(self.time_stamps) 
- 
-        cpu_query = top_c.aggregate([{ "$match" : {'time' : {'$lte' : end_time.timestamp,"$gte" : start_time.timestamp}}},{"$group":{"_id": "$identifier" ,'data': { "$push": "$cpu"}}}])
-        
-        self.cpu_results = []          
-        for cpu_reading in cpu_query['result']: 
-            self.cpu_results.append(cpu_reading) 
-
-        #this gets a little bigger than the largest memory use, so we can add a y axis on our graph
-        self.ram_maximum = 1.2 * top_c.find_one(sort=[("rss",-1)])['rss'] 
-
-        ram_query = top_c.aggregate([{ "$match" : {'time' : {'$lte' : end_time.timestamp,"$gte" : start_time.timestamp}}},{"$group":{"_id": "$identifier" ,'data': { "$push": "$rss"}}}])
-        
-        self.ram_results = [] 
-        for ram_reading in ram_query['result']: 
-            self.ram_results.append(ram_reading) 
-
-#######################################################################
-#Initialize some test queries, in the future this should be in a method.
+#############################
+#Initialize some test queries 
+############################
 
 name = "Brofiler"
 start = arrow.get('2014-05-11T21:23:58.970460+00:00')
 end  = arrow.get('2015-05-11T21:23:58.970460+00:00')
-top_sample = top_graph(start,end)
+top_sample = top_graph(start,end,top_c)
 netstat_sample = netstat_graph(start,end,netstat_c)
 capstat_sample = capstat_graph(start,end,capstat_c)
 
